20edf29f1e1dc51c5cb84cf86157c691176c590b
anahi <anahikattan1991@gmail.com>
2026-01-05 02:58:00 -0300

    Corregir imagenes de galeria diferenciadas para Puerto Bandera y Combo Moreno

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -135,7 +135,7 @@ export const TRASLADOS_DATA: ServiceItem[] = [
         image: '/images/puerto-bandera-cover.jpg',
         gallery: [
             '/images/puerto-bandera-cover.jpg',
-            '/images/puerto-bandera-glaciar-personas.jpg',
+            '/images/puerto-bandera-aerea.jpg',
             '/images/puerto-bandera-hielo.jpg',
             '/images/puerto-bandera-cartel.jpg'
         ],
@@ -158,7 +158,7 @@ export const TRASLADOS_DATA: ServiceItem[] = [
             '/images/puerto-bandera-hielo.jpg',
             '/images/puerto-bandera-cartel.jpg',
             '/images/puerto-bandera-cover.jpg',
-            '/images/puerto-bandera-aerea.jpg'
+            '/images/puerto-bandera-glaciar-personas.jpg'
         ],
         category: 'traslados',
         galleryKeywords: 'boat glacier lake navigation icebergs',

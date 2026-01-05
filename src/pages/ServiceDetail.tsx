4f60c55ee41bfcb6ef2b3f361d4881948ab1ac23
anahi <anahikattan1991@gmail.com>
2026-01-05 01:29:05 -0300

    Ajustar encuadre de portada para Chaltén Full Day (object-top)

--- src/pages/ServiceDetail.tsx
+++ src/pages/ServiceDetail.tsx
@@ -65,7 +65,7 @@ const ServiceDetail: React.FC = () => {
                 <img
                     src={service.image}
                     alt={service.title}
-                    className="absolute inset-0 w-full h-full object-cover animate-in fade-in duration-700"
+                    className={`absolute inset-0 w-full h-full object-cover animate-in fade-in duration-700 ${service.id === 'exc-chalten-full' ? 'object-top' : 'object-center'}`}
                 />
 
                 {/* Gradient Bleed Effect */}
